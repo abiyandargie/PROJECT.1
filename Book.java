0aaf656a41d4769bd277bc07395fd42fe43cffd3
HermelaDemeke <bhmd0595@gmail.com>
2023-12-02 11:25:22 +0300

    modified version of class : Book , Classroom,Teacher

--- Book.java
+++ Book.java
@@ -5,7 +5,7 @@ public class Book {
     String title;
     String author;
     List<Rental> rentals;
-    private String Isbn;
+    final private String Isbn;
 
     public Book(String title, String author,String Isbn) {
         this.title = title;
@@ -15,29 +15,14 @@ public class Book {
 
     }
 
-    public void setIsbn(String isbn) {
-        Isbn = isbn;
-    }
 
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getAuthor() {
         return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public List<Rental> getRentals() {
-        return rentals;
     }
 
     public void addRental(Rental rental) {
--- Teacher.java
+++ Teacher.java
@@ -1,13 +1,8 @@
 public class Teacher extends Person {
      String specialization;
-     boolean parent_permission = true;
-
     public Teacher(int id, String name, boolean parent_permission, String specialization,int age) {
         super(Integer.parseInt(String.valueOf(id)), name, parent_permission,age);
         this.specialization = specialization;
     }
 
-    public boolean can_use_services() {
-        return true;
-    }
 }
